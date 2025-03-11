65991a3b88ff015c896263c25f44c7e6b5bb9d59
Abhi7033 <abhishekanand3860@gmail.com>
2025-03-11 16:06:27 +0530

    added jwt implementation

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "randomabhishek"
 const app = express();
 
 app.use(express.json());
@@ -10,16 +12,17 @@ const users =  [];
 //]
 
 //should return a random long string
-function generateToken() {
-    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+//we should use jwt instead of this
+// function generateToken() {
+//     let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-    let token = "";
-    for (let i = 0; i < 32; i++) {
-        // use a simple function here
-        token += options[Math.floor(Math.random() * options.length)];
-    }
-    return token;
-}
+//     let token = "";
+//     for (let i = 0; i < 32; i++) {
+//         // use a simple function here
+//         token += options[Math.floor(Math.random() * options.length)];
+//     }
+//     return token;
+// }
 
 app.post("/signup", function(req, res){
     //input validation using zod -> later
@@ -52,8 +55,11 @@ app.post("/signin", function(req, res){
     })
 
     if(foundUser){
-        const token = generateToken();
-        foundUser.token = token;
+         //convert their username over to a jwt
+        const token = jwt.sign({
+            username : username
+        }, JWT_SECRET); 
+        // foundUser.token = token;  //no need to store it in memory variable
         res.json({
             token: token
         })
@@ -69,9 +75,12 @@ app.post("/signin", function(req, res){
 })
 
 app.get("/me", function(req, res){
-    const token = req.header.token;
+    const token = req.header.token; //jwt
+    const decodedInfo = jwt.verify(token , JWT_SECRET); // we will get the decoded username here {username : abc} 
+    const username = decodedInfo.username;
+
     const foundUser = users.find(function(u){
-        if(u.token == token){
+        if(u.username == username){
             return true;
         }else{
             return false;
